8b9716051dc0e0b766e099df8d0e67887f8a5f31
Nicos Angelopoulos <nicos@gmx.co.uk>
2022-01-05 12:49:17 +0000

    ADDED  comments and coa_all (e=1...10)

--- run/gbns_in_cancer/coa.pl
+++ run/gbns_in_cancer/coa.pl
@@ -7,6 +7,13 @@
 
 :- debug(coa).
 
+/** coa
+
+Run GBN on the colorectal cancer dataset with \epsilon = 1 producing all outputs
+as shown on GBN in cancer paper.
+
+*/
+
 coa :-
     debug_call( coa, start, true ),
     DatF = pack('gbn/data/gbns_in_cancer/coa/coa_min05.dat'),
@@ -15,6 +22,30 @@ coa :-
                 setting(edge_penalty,E),
                 dir(OsOdir)
                 % debug(true)
+              ],
+    gbn( GBNOpts ),
+        debug_call( coa, start, fisher_nets ),
+    gbn_fisher_nets( dir(OsOdir) ),    
+        debug_call( coa, start, fam_hmaps ),
+    gbn_fam_hmaps( dir(OsOdir) ),   % only for binaries
+        debug_call( coa, start, gates_nets ),
+    gbn_gates_nets( dir(OsOdir) ),
+        debug_call( coa, start, svg_legend ),
+    gbn_svg_legend( dir(OsOdir) ),
+        debug_call( coa, end, true ).
+    
+/** coa_all.
+
+Run GBN on the colorectal cancer dataset for \epsilon =1...10.
+
+*/
+coa_all :-
+    debug_call( coa, start, true ),
+    DatF = pack('gbn/data/gbns_in_cancer/coa/coa_min05.dat'),
+    numlist( 1, 10, Es ),
+    GBNOpts = [ data(DatF),
+                multiple(edge_penalty,Es),
+                dir(OsOdir)
               ],
     gbn( GBNOpts ),
         debug_call( coa, start, fisher_nets ),
