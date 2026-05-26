bd62ad695a382b649dd155fd7abd62a8c975813b
Nicos Angelopoulos <nicos@gmx.co.uk>
2026-05-26 15:23:25 +0100

    OPTS:    palim(4), sets the old GOBNILP behaviour as default (the new GOBNILP def is no constraint)

--- src/gbn.pl
+++ src/gbn.pl
@@ -21,14 +21,15 @@ D = date(2021, 1, 23).
 gbn_version(0:2:1, date(2026,5,25)).
 
 gbn_defaults( Defs ) :-
-	Defs = [ % dir(Gob),
+     Defs = [ % dir(Gob),
               adjmat(false),
               copy(false),
               discrete(true),
-		    data(pack('gbn/data/asia.bnsl')),
-		    display_dot(svg),
-	         std_output(std_file)
-	       ].
+              data(pack('gbn/data/asia.bnsl')),
+              display_dot(svg),
+              palim(4),
+              std_output(std_file)
+            ].
 
 /** gbn.
     gbn(Options).
@@ -39,6 +40,7 @@ Options is a term or list of:
 
   * adjmat(AdjMat=false)
      if not ==false== the the adjmatrix is outputed (if ==true== to <Pbname>_adjmat.txt, else AdjMat is take as the full file name).
+
   * copy(Copy=false)
     if set to anything else than false the local file Copy is copied into Dir- multiple are allowed
 
@@ -56,6 +58,10 @@ Options is a term or list of:
 
   * multiple(Setting,Vals)
     run multiple experiments where setting is given each Val-ue in turn
+
+  * palim(PaLim=4)
+    max no of parents allowed. Used to be 4 in GOBNILP but now is unrestricted which makes finding the best model a lot harder for bigger problems.
+    Set to -1 to remove the constraint.
 
   * probname(Pbname)
     used as stem for output files. Defaults is the stem of Dfile (so for Dfile =../data/asia.bnsl the stem is _asia_).
@@ -76,191 +82,195 @@ Options is a term or list of:
 */
 
 gbn :-
-	gbn( [] ).
+     gbn( [] ).
 
 gbn( Args ) :-
-	options_append( gbn, Args, All, [process(debug),debug(true),module(gbn)] ),
-	debuc( gbn(gbn), 'Options: ~w', [All] ),
-	selectchk( data(Dfile), All, NtAll ),
-	absolute_file_name( Dfile, AbsDfile ),
-	file_base_name( AbsDfile, DBname ),
+     options_append( gbn, Args, All, [process(debug),debug(true),module(gbn)] ),
+     debuc( gbn(gbn), 'Options: ~w', [All] ),
+     selectchk( data(Dfile), All, NtAll ),
+     absolute_file_name( Dfile, AbsDfile ),
+     file_base_name( AbsDfile, DBname ),
 
-	gbn_prob_name( NtAll, ProbName, DBname, NpAll ),
+     gbn_prob_name( NtAll, ProbName, DBname, NpAll ),
 
-	gbn_out_dir( ProbName, Dir, All ),
-	make_directory( Dir ),
+     gbn_out_dir( ProbName, Dir, All ),
+     make_directory( Dir ),
 
-	directory_file_path( Dir, DBname, DatTarget ),
-	copy_file( AbsDfile, DatTarget ),
-    findall( _, (member(copy(Copy),All),
+     directory_file_path( Dir, DBname, DatTarget ),
+     copy_file( AbsDfile, DatTarget ),
+     findall( _, (member(copy(Copy),All),
                    ( Copy == false -> true; @cp(Copy,Dir) )
                 ),
                     _ ),
-	% rename_file( SetsF, Dir ),
+     % rename_file( SetsF, Dir ),
 
-	debuc( gbn(gbn), 'Output directory: ~p', [Dir] ),
-	working_directory( Old, Dir ),
-	gob_prob_name_settings( ProbName, DBname, SetsF, All ),
-	gbn_multiples( ProbName, DBname, SetsF, NpAll ),
+     debuc( gbn(gbn), 'Output directory: ~p', [Dir] ),
+     working_directory( Old, Dir ),
+     gob_prob_name_settings( ProbName, DBname, SetsF, All ),
+     gbn_multiples( ProbName, DBname, SetsF, NpAll ),
 
-	working_directory( _, Old ).
+     working_directory( _, Old ).
 
 gbn_singleton( ProbName, DBname, SetsF, All ) :-
-	debuc( gbn(gbn), 'Settings on: ~w', SetsF ),
-	% atomic_list_concat( ['-s ',SetsF], Garg ),
-	selectchk( std_output(Sout), All, _NsAll ),
-	std_output_gobnilp( Sout, SetsF, ProbName, DBname ),
-	options( display_dot(DispDot), All ),
-	gbn_singleton_display_dot( DispDot, SetsF ).
+     debuc( gbn(gbn), 'Settings on: ~w', SetsF ),
+     % atomic_list_concat( ['-s ',SetsF], Garg ),
+     selectchk( std_output(Sout), All, _NsAll ),
+     std_output_gobnilp( Sout, SetsF, ProbName, DBname ),
+     options( display_dot(DispDot), All ),
+     gbn_singleton_display_dot( DispDot, SetsF ).
 
 gbn_singleton_display_dot( false, _SetsF ) :-
-	!.
+     !.
 % fixme: allow Ext that are not the same as their format
 gbn_singleton_display_dot( Ext, SetsF ) :-
-	os_ext( set, dot, SetsF, DotF ),
-	os_ext( set, Ext, SetsF, ImgF ),
-	atom_concat( '-T', Ext, TForm ),
-	% gobnilp does not quote specials, so HLA-A trips the dot exec.
-	% pupsh is my local script for fixing those files by replacing with "HLA-A"
-	% maybe we can do such calls via options ... or hooks
-	% ( once(which(pupsh,_)) ->
+     os_ext( set, dot, SetsF, DotF ),
+     os_ext( set, Ext, SetsF, ImgF ),
+     atom_concat( '-T', Ext, TForm ),
+     % gobnilp does not quote specials, so HLA-A trips the dot exec.
+     % pupsh is my local script for fixing those files by replacing with "HLA-A"
+     % maybe we can do such calls via options ... or hooks
+     % ( once(which(pupsh,_)) ->
     dot_fix( DotF ),
     ( which(dot,_DotEx) ->
-	    @ dot( TForm, -o, ImgF, DotF )
+         @ dot( TForm, -o, ImgF, DotF )
         ; 
         % fixme: create info channel ?
         gbn_message( exec_miss(dot,'cannot create net',DotF) )
     ).
 
 gbn_multiples( ProbName, DBname, SetsF, All ) :-
-	\+ memberchk( multiple(_,_), All ),
-	!,
-	gbn_singleton( ProbName, DBname, SetsF, All ).
+     \+ memberchk( multiple(_,_), All ),
+     !,
+     gbn_singleton( ProbName, DBname, SetsF, All ).
 gbn_multiples( ProbName, DBname, SetsF, All ) :-
-	% gbn_singleton( ProbName, DBname, SetsF, All ),
-	findall( t(Key,Ktkn,Vals), 
-					( member(multiple(Key,Vals),All),
-					  ( memberchk(setting_token(Key,Ktkn),All) ->
-					  	true
-						;
-						downcase_atom(Key,Ley),
-						sub_atom(Ley,0,1,_,Ktkn)
-					  )
-					), KKVss ),
-	exclude( form_term(multiple/2), All, Opts ),
-	once((gbn_multiples_loop(KKVss,ProbName,DBname,SetsF,'',Opts);true)).
+     % gbn_singleton( ProbName, DBname, SetsF, All ),
+     findall( t(Key,Ktkn,Vals), 
+                         ( member(multiple(Key,Vals),All),
+                           ( memberchk(setting_token(Key,Ktkn),All) ->
+                              true
+                              ;
+                              downcase_atom(Key,Ley),
+                              sub_atom(Ley,0,1,_,Ktkn)
+                           )
+                         ), KKVss ),
+     exclude( form_term(multiple/2), All, Opts ),
+     once((gbn_multiples_loop(KKVss,ProbName,DBname,SetsF,'',Opts);true)).
 
 gbn_multiples_loop( [], ProbName, DBname, SetsF, StemExt, All ) :-
-	os_postfix( StemExt, SetsF, ToSetsF, sep(-) ),
-	@ cp( -i, SetsF, ToSetsF ),
-	gbn_settings_file_append( ToSetsF, All ),
-	gbn_singleton( ProbName, DBname, ToSetsF, [display_dot(false)|All] ),
-	% fixme: change svg, to DispDot option
-	maplist( gbn_multiple_rename(SetsF,ToSetsF), [dot,bn] ),
-	options( display_dot(DispDot), All ),
-	gbn_singleton_display_dot( DispDot, ToSetsF ).
+     os_postfix( StemExt, SetsF, ToSetsF, sep(-) ),
+     @ cp( -i, SetsF, ToSetsF ),
+     gbn_settings_file_append( ToSetsF, All ),
+     gbn_singleton( ProbName, DBname, ToSetsF, [display_dot(false)|All] ),
+     % fixme: change svg, to DispDot option
+     maplist( gbn_multiple_rename(SetsF,ToSetsF), [dot,bn] ),
+     options( display_dot(DispDot), All ),
+     gbn_singleton_display_dot( DispDot, ToSetsF ).
 
 gbn_multiples_loop( [t(Key,Tkn,Vals)|T], ProbName, DBname, SetsF, StemExt, Opts ) :-
-	member( Val, Vals ),
-	atomic_list_concat( [StemExt,Tkn,Val], NxtExt ),
-	All = [setting(Key,Val)|Opts],
-	gbn_multiples_loop( T, ProbName, DBname, SetsF, NxtExt, All ),
-	fail.
+     member( Val, Vals ),
+     atomic_list_concat( [StemExt,Tkn,Val], NxtExt ),
+     All = [setting(Key,Val)|Opts],
+     gbn_multiples_loop( T, ProbName, DBname, SetsF, NxtExt, All ),
+     fail.
 
 gbn_settings_file_append( SetsF, Opts ) :-
-	open( SetsF, append, Out ),
-	findall( _, (member(setting(Key,Val),Opts),gob_stream_setting(Out,Key,Val)), _ ),
-	close( Out ).
+     open( SetsF, append, Out ),
+     findall( _, (member(setting(Key,Val),Opts),gob_stream_setting(Out,Key,Val)), _ ),
+     close( Out ).
 
 form_term( Name/Arity, Term ) :-
-	functor( Term, Name, Arity ).
+     functor( Term, Name, Arity ).
 
 gbn_multiple_rename( SetsF, ToSetsF, Ext ) :-
-	os_ext( Old, Ext, SetsF, From ),
-	os_ext( Old, Ext, ToSetsF, To ),
-	@ mv( -i, From, To ).
+     os_ext( Old, Ext, SetsF, From ),
+     os_ext( Old, Ext, ToSetsF, To ),
+     @ mv( -i, From, To ).
 
 gbn_out_dir( _ProbName, Dir, All ) :-
-	memberchk( dir(Dir), All ),
-	ground( Dir ),
-	!.
+     memberchk( dir(Dir), All ),
+     ground( Dir ),
+     !.
 gbn_out_dir( _ProbName, Dir, Opts ) :-
-	memberchk( dir_prefix(Pfx), Opts ),
-	!,
-	os_unique( Pfx, Dir, [] ).
+     memberchk( dir_prefix(Pfx), Opts ),
+     !,
+     os_unique( Pfx, Dir, [] ).
 gbn_out_dir( ProbName, Dir, Opts ) :-
-	os_unique( ProbName, Dir, [create(false)] ),
-	( memberchk(dir(Dir),Opts) -> true % returns it
-	                            ; true ).
+     os_unique( ProbName, Dir, [create(false)] ),
+     ( memberchk(dir(Dir),Opts) -> true % returns it
+                                 ; true ).
 
 std_output_gobnilp( output, SetsF, _Prob, DBname ) :-
-	@ gobnilp( '-s', SetsF, '-f', DBname ).
+     @ gobnilp( '-s', SetsF, '-f', DBname ).
 std_output_gobnilp( std_file, SetsF, Prob, DBname ) :-
-	atom_concat( Prob, '_std_output', ProbStdOut ),
-	file_name_extension( ProbStdOut, txt, PSOf ),
-	!,
-	file_ouput_gobnilp( PSOf, SetsF, DBname ).
+     atom_concat( Prob, '_std_output', ProbStdOut ),
+     file_name_extension( ProbStdOut, txt, PSOf ),
+     !,
+     file_ouput_gobnilp( PSOf, SetsF, DBname ).
 
 file_ouput_gobnilp( OutF, SetsF, DBname ) :-
-	Outs @@ gobnilp( '-s', SetsF, '-f', DBname ),
-	open( OutF, write, Out ),
-	maplist( writenl(Out), Outs ),
-	close( Out ).
+     Outs @@ gobnilp( '-s', SetsF, '-f', DBname ),
+     open( OutF, write, Out ),
+     maplist( writenl(Out), Outs ),
+     close( Out ).
 
 writenl( Out, Line ) :-
-	write( Out, Line ), nl( Out ).
+     write( Out, Line ), nl( Out ).
 
 % file_name_extension( ProbName, bn, BnF ),
 
 gbn_prob_name( All, ProbName, _DBname, Red ) :-
-	select( probname(ProbName), All, Red ),
-	!.
+     select( probname(ProbName), All, Red ),
+     !.
 gbn_prob_name( All, ProbName, DBname, All ) :-
-	file_name_extension( ProbName, _Est, DBname ).
-	
+     file_name_extension( ProbName, _Est, DBname ).
+     
 gob_prob_name_settings( ProbName, _DBname, SetsF, Opts ) :-
-	file_name_extension( ProbName, set, SetsF ),
-	open( SetsF, write, Out ),
+     file_name_extension( ProbName, set, SetsF ),
+     open( SetsF, write, Out ),
      % fixme: the following two have disappeared 26.05.25
-	% gob_stream_setting( Out, 'nbns', 1 ),
-	% gob_stream_setting( Out, 'outputfile/solution', ProbName, bn ),
-	% gob_stream_setting( Out, 'outputfile/solution', '"ran-asia/asia.bn"' ),
+     % gob_stream_setting( Out, 'nbns', 1 ),
+     % gob_stream_setting( Out, 'outputfile/solution', ProbName, bn ),
+     % gob_stream_setting( Out, 'outputfile/solution', '"ran-asia/asia.bn"' ),
      ( options(discrete(true), Opts) ->
           gob_stream_setting( Out, score_type, 'BDeu' )
           ;
           true
           % gob_stream_setting( Out, score_type, 'GaussianL0' )
      ),
-	gob_stream_setting( Out, 'outputfile/dotfile', ProbName, dot ),
-	atom_concat( ProbName, '_scnti', Scnti ),
-	gob_stream_setting( Out, 'outputfile/scoreandtimefile', Scnti, txt ),
+     gob_stream_setting( Out, 'outputfile/dotfile', ProbName, dot ),
+     atom_concat( ProbName, '_scnti', Scnti ),
+     gob_stream_setting( Out, 'outputfile/scoreandtimefile', Scnti, txt ),
      options( adjmat(AdjMat), Opts ),
-     gob_prob_name_setting( AdjMat, ProbName, AmF ),
-	gob_stream_setting( Out, 'outputfile/adjacencymatrixfile', AmF ),
-	findall( _, (member(setting(Key,Val),Opts),gob_stream_setting(Out,Key,Val)), _ ),
-	close( Out ).
+     gob_prob_name_setting_adjmat( AdjMat, ProbName, AmF ),
+     gob_stream_setting( Out, 'outputfile/adjacencymatrixfile', AmF ),
+     options( palim(PaLim), Opts ),
+     gob_stream_setting( Out, 'palim', PaLim ),
+     findall( _, (member(setting(Key,Val),Opts),gob_stream_setting(Out,Key,Val)), _ ),
+     close( Out ).
 
-gob_prob_name_setting( false, _, '' ) :- !.
-gob_prob_name_setting( true, ProbName, AmF ) :- 
+gob_prob_name_setting_adjmat( false, _, '' ) :- !.
+gob_prob_name_setting_adjmat( true, ProbName, AmF ) :- 
      !,
      atomic_list_concat( [ProbName,'adjmat.txt'], '_', AmF ).
-gob_prob_name_setting( Else, _ProbName, Else ).
+gob_prob_name_setting_adjmat( Else, _ProbName, Else ).
 
 gob_stream_setting( Out, Key, Value ) :-
-	write( Out, 'gobnilp/' ), 
-	write( Out, Key ), 
-	write( Out, ' = ' ),
-	( number(Value) -> write(Out,Value); 
-				    write(Out,'"' ),
-				    write(Out,Value),
-				    write(Out,'"' ) ),
-	nl( Out ).
+     write( Out, 'gobnilp/' ), 
+     write( Out, Key ), 
+     write( Out, ' = ' ),
+     ( number(Value) -> write(Out,Value)
+                        ; 
+                        write(Out,'"' ),
+                        write(Out,Value),
+                        write(Out,'"' ) 
+     ),
+     nl( Out ).
 gob_stream_setting( Out, Key, Value, Ext ) :-
-	write( Out, 'gobnilp/' ),
-	write( Out, Key ),
-	write( Out, ' = "' ),
-	write( Out, Value ),
-	write( Out, '.' ),
-	write( Out, Ext ),
-	write( Out, '"' ),
-	nl( Out ).
+     write( Out, 'gobnilp/' ),
+     write( Out, Key ),
+     write( Out, ' = "' ),
+     write( Out, Value ),
+     write( Out, '.' ),
+     write( Out, Ext ),
+     write( Out, '"' ),
+     nl( Out ).
